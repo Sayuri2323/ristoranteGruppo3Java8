24729b49f847a9e455318b605b900fe0ba976871
RaffaeleCuccaro1234 <118295719+RaffaeleCuccaro1234@users.noreply.github.com>
2022-12-20 15:33:10 +0100

    diversificazione bevande  20/12

--- src/it/ristoranteGruppo3/entities/portate/Bevanda.java
+++ src/it/ristoranteGruppo3/entities/portate/Bevanda.java
@@ -9,7 +9,7 @@ import it.ristoranteGruppo3.entities.enums.TypeEnum;
  */
 
 public class Bevanda extends Portata {
-
+    private boolean hasAlcohol;
     /**
      * costruttore All args
      * @param namePortata Nome della portata
@@ -17,17 +17,20 @@ public class Bevanda extends Portata {
      * @param typeEnum tipo della portata
      * @param descrizione descrizione della portata
      */
-    public Bevanda(String namePortata, double pricePortata, TypeEnum typeEnum,String descrizione) {
+
+    public Bevanda(String namePortata, double pricePortata, TypeEnum typeEnum,boolean hasAlcohol,String descrizione) {
         super(namePortata, pricePortata, typeEnum,descrizione);
+        this.hasAlcohol = hasAlcohol;
     }
 
     /**
-     * Costruttore per tutte le portate che non necessitano di una descrizione
+     * Costruttore per tutte le bevande senza descrizione o contenuto alcolico
      * @param namePortata Nome della portata
      * @param pricePortata prezzo della portata
      * @param typeEnum tipo della portata
      */
     public Bevanda(String namePortata, double pricePortata, TypeEnum typeEnum) {
         super(namePortata, pricePortata, typeEnum);
+        hasAlcohol = false;
     }
 }
