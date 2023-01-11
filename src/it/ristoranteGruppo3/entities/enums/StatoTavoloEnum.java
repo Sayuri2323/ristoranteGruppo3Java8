bccad0291ef53c9f09ca1f8c76867502df7b57cb
FrancescoSepe98 <sepe.francesco.lavoro@gmail.com>
2023-01-11 23:52:11 +0100

    Inserite le desrcizioni all'enumerazione dello stato del tavolo

--- src/it/ristoranteGruppo3/entities/enums/StatoTavoloEnum.java
+++ src/it/ristoranteGruppo3/entities/enums/StatoTavoloEnum.java
@@ -4,7 +4,12 @@ public enum StatoTavoloEnum {
     /**
      * TODO aggiungere descrizioni agli elementi enum
      */
-    LIBERO,
-    OCCUPATO,
-    RISERVATO
+    LIBERO("Il tavolo è libero"),
+    OCCUPATO("Il tavolo è Occupato");
+
+    private String StatoTavolo;
+    StatoTavoloEnum (String s){
+        this.StatoTavolo = s;
+    }
+
 }
