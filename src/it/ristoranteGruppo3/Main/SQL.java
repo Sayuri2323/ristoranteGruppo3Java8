40b3712b0d88065b8c7aa98a299e9f46effd9a72
FrancescoSepe98 <sepe.francesco.lavoro@gmail.com>
2023-01-31 15:00:39 +0100

    Fix main Class

--- src/it/ristoranteGruppo3/Main/SQL.java
+++ src/it/ristoranteGruppo3/Main/SQL.java
@@ -1,14 +1,11 @@
 import it.ristoranteGruppo3.databaseUtility.*;
-import it.ristoranteGruppo3.databaseUtility.SqlTableFactory.ClientDBInjection;
-import it.ristoranteGruppo3.databaseUtility.SqlTableFactory.SqlTableGenerator;
+
 
 import java.sql.SQLException;
 
 public class SQL {
     public static void main(String[] args) throws SQLException {
-        DB db = DB.getInstance("root","password","ristorante_gruppo_3");
-    SqlTableGenerator gen  = ClientDBInjection.createSqlTableGenerator();
-    gen.createTable(db.getConnectionDB());
-
+        DB database = DB.getInstance("root","password","ristorante_gruppo_3");
+        database.createNewDB();
     }
 }
--- src/it/ristoranteGruppo3/databaseUtility/SqlTableFactory/ClientDBInjection.java
+++
@@ -1,9 +0,0 @@
-package it.ristoranteGruppo3.databaseUtility;
-
-public class SqlTableFactory {
-    public static SqlTableGenerator createSqlTableGenerator(){
-        SqlTableGenerator STG = new SqlTableGenerator();
-        STG.setTable(new ClientDB());
-        return STG;
-    }
-}
