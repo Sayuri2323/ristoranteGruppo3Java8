433e9312b3c07f162647b88ff6af0f3814c8d312
RaffaeleCuccaro1234 <118295719+RaffaeleCuccaro1234@users.noreply.github.com>
2023-02-07 17:33:11 +0100

    errore di battitura

--- src/it/ristoranteGruppo3/databaseUtility/RestaurantTable.java
+++ src/it/ristoranteGruppo3/databaseUtility/RestaurantTable.java
@@ -36,6 +36,6 @@ public class RestaurantTable implements ISqlTable{
     public void insertInto(Connection connection, String restaurant_name,String restaurant_Address,int max_capacity,String web_site, int phone_number) throws SQLException {
         Statement statement = connection.createStatement();
         statement.executeUpdate("INSERT INTO `test`.`restaurant` (`restaurant_name`, `restaurant_Address`, `max_capacity`, `web_site`, `phone_number`) VALUES ('"+ restaurant_name +"', '"+restaurant_Address + "', '"+ max_capacity +"', '"+web_site+"', '"+ phone_number +"');\n");
-   }double
+   }
     }
 
